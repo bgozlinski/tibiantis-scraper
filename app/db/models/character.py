90119dd0aa397fa514efc4de9c7903332b0f3249
bgozlinski <BARTLOMIEJ.GOZLINSKI@GMAIL.COM>
2025-05-22 12:00:29 +0200

    first commit

---
+++ app/db/models/character.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, DateTime, Text
+from app.db.models.base import Base
+from datetime import datetime, UTC
+
+
+class Character(Base):
+    __tablename__ = "characters"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    sex = Column(String, nullable=True)
+    vocation = Column(String, nullable=True)
+    level = Column(Integer, nullable=True)
+    world = Column(String, nullable=True)
+    residence = Column(String, nullable=True)
+    house = Column(String, nullable=True)
+    guild_membership = Column(String, nullable=True)
+    last_login = Column(DateTime, nullable=True)
+    comment = Column(Text, nullable=True)
+    account_status = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.now(UTC))
+    updated_at = Column(DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC))
---
+++ app/db/session.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+engine = create_engine(os.getenv("DB_URL"))
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
