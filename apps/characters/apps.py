8812477853ca7b4551a52d503436981cb10fa5e9
bgozlinski <bartlomiej.gozlinski@gmail.com>
2026-04-17 21:15:24 +0200

    fix(characters): drop duplicate default_auto_field from AppConfig

--- apps/characters/apps.py
+++ apps/characters/apps.py
@@ -4,4 +4,3 @@ from django.apps import AppConfig
 class CharactersConfig(AppConfig):
     name = "apps.characters"
     label = "characters"
-    default_auto_field = "django.db.models.BigAutoField"
